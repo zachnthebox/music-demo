e5ddf1c60560bb4707333655be02cf199f1ab9ae
zachnthebox <zachary@zachnthebox.com>
2015-09-15 19:09:47 -0400

    Changes

--- app/controllers/music/album.js
+++ app/controllers/music/album.js
@@ -2,25 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	actions: {
-		addSong: function(song) {
-			var album = this.store.createRecord("album", {
-				title: song.album,
-				artist: song.artist
-			});
-			var songRecord = this.store.createRecord("song", {
+		addSong: function(song, album) {
+			var song = this.store.createRecord("song", {
 				title: song.title,
 				artist: song.artist,
 				album: album,
 			});
 
-			album.get("songs").pushObject(songRecord);
+			album.get("songs").pushObject(song);
 
 			album.save();
-			songRecord.save();
+			song.save();
 		},
 		removeSong: function(song) {
-			song.deleteRecord();
-			song.save();
+			var album = song.get("album");
+			album.removeObject(song);
+			album.save();
+
+			song.destroyRecord();
 		}
 	}
 });

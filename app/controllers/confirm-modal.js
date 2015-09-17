9b5a28ac9e699f09fd8b1db97a2e01d2c7981d32
zachnthebox <zachary@zachnthebox.com>
2015-09-16 22:37:34 -0400

    Added back arrow in the app bar

--- app/controllers/confirm-modal.js
+++ app/controllers/confirm-modal.js
@@ -1,19 +1,19 @@
 import ModalController from 'mustache-you/controllers/modal';
 
 export default ModalController.extend({
-  actions: {
-    cancel: function() {
-      debugger;
-      if (this.content) {
-        this.content.rollback();
-      }
-      return this.send('closeModal');
-    },
-    okay: function() {
-      debugger;
-      this.$('.modal').modal('hide');
-      this.sendAction('okay');
-      return true;
-    }
-  }
+	actions: {
+		cancel: function() {
+			debugger;
+			if (this.content) {
+				this.content.rollback();
+			}
+			return this.send('closeModal');
+		},
+		okay: function() {
+			debugger;
+			this.$('.modal').modal('hide');
+			this.sendAction('okay');
+			return true;
+		}
+	}
 });
--- app/routes/music/album.js
+++ app/routes/music/album.js
@@ -5,11 +5,18 @@ export default Ember.Route.extend({
 		var musicController = this.controllerFor("music");
 		musicController.set("selectedAlbum", params.album_id);
 		musicController.set("isDetail", true);
+
+		var applicationController = this.controllerFor("application");
+		applicationController.set("backRoute", "music");
+
 		return this.store.find("album", params.album_id);
 	},
 	deactivate: function() {
 		var musicController = this.controllerFor("music");
 		musicController.set("selectedAlbum", this.paramsFor().album_id);
 		musicController.set("isDetail", false);
+
+		var applicationController = this.controllerFor("application");
+		applicationController.set("backRoute", undefined);
 	}
 });

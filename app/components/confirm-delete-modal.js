ee981a440902df9fa261ce039fcb7d414ad60a60
zachnthebox <Zachary.Berardo@kub.org>
2015-09-17 09:20:15 -0400

    remove "bad" work
    reverted direction

--- app/components/confirm-delete-modal.js
+++
@@ -1,6 +0,0 @@
-import ConfirmModalView from 'mustache-you/components/confirm-modal';
-
-export default ConfirmModalView.extend({
-  message: "Are you sure you want to delete this?",
-  title: "Confirm Delete"
-});

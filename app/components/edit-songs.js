4c355b8d7674b28d4493ba2ec776b5f5ea800555
zachnthebox <Zachary.Berardo@kub.org>
2015-09-18 16:11:02 -0400

    fix null references

--- app/components/edit-songs.js
+++ app/components/edit-songs.js
@@ -4,7 +4,8 @@ export default Ember.Component.extend({
 	hasError: false,
 
 	defaultArtist: function() {
-		return this.get("album").get("artist");
+		var album = this.get("album");
+		return album && album.get("artist");
 	}.property("album"),
 
 	artistPlaceholder: function() {
@@ -16,7 +17,8 @@ export default Ember.Component.extend({
 	songToAdd: undefined,
 
 	songs: function() {
-		return this.get("album").get("songs");
+		var album = this.get("album");
+		return album && album.get("songs") || [];
 	}.property("album"),
 
 	album: undefined,
